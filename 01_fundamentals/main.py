0e5fbfaee99dca4c7ff800bdff2eac606b8be303
Mirzahan Akın <mirzahanakin@gmail.com>
2026-03-01 00:34:35 +0300

    01_fundamentals

---
+++ 01_fundamentals/main.py
@@ -0,0 +1,36 @@
+from random import randrange
+from typing import Optional
+from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
+
+app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1}, {
+    "title": "favorite foods", "content": "I like pizza", "id": 2}]
+
+@app.get("/")
+def root():
+    return {"message": "Hello World!"}
+
+@app.get("/posts")
+def get_posts():
+    return {"data": my_posts}
+
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    my_posts.append(post_dict)
+    return {"data": my_posts}
+
+@app.get("/posts/{id}")
+def get_post(id):
+    print(id)
+    return {"post_detail": f"Here is post {id}"}
