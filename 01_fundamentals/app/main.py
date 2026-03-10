f9716f1c89be38c97c70918cb035422ec4dddcc1
Mirzahan Akın <mirzahanakin@gmail.com>
2026-03-11 02:29:30 +0300

    fundamentals finished

--- 01_fundamentals/app/main.py
+++ 01_fundamentals/app/main.py
@@ -19,6 +19,11 @@ def find_posts(id):
     for p in my_posts:
         if p['id'] == id:
             return p
+    
+def find_index_post(id):
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 @app.get("/")
 def root():
@@ -28,7 +33,7 @@ def root():
 def get_posts():
     return {"data": my_posts}
 
-@app.post("/posts")
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0, 1000000)
@@ -42,5 +47,33 @@ def get_post(id, response: Response):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail= f"post with id: {id} was not found")
         # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {'message': f"post with id: {id} was not found"}
+        # return {'message': f"post with id: {id} was not found"}   
     return {"post_detail": f"Here is post {id}"}
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    # deleting post
+    # find the index in the array that has required ID
+    # my_posts.pop(index)
+    index = find_index_post(id)
+    
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post wih id: {id} does not exist")
+
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id: int, post: Post):
+    print(post)
+    index = find_index_post(id)
+    
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post wih id: {id} does not exist")
+    
+    post_dict = post.dict()
+    post_dict['id'] = id
+    my_posts[index] = post_dict
+    return {'data': post_dict}
